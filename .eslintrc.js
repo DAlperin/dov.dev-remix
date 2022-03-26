b3a6437120c946ec7aec570bf14d304246b81b65
Dov Alperin <git@dov.dev>
2022-03-26 19:09:30 -0400

    Disable unhelpful eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,6 +2,12 @@ module.exports = {
     extends: "galex",
     ignorePatterns: ["build", "public/build/**"],
     rules: {
+        // Tailwind configuration...
         "import/dynamic-import-chunkname": 0,
+        // Move fast, break things, etc, etc, etc
+        "unicorn/no-abusive-eslint-disable": 0,
+        // In Remix, we can throw a RuntimeRedirect to bubble up a redirect to a loader or action
+        "@typescript-eslint/no-throw-literal": 0,
+        "curly": 0
     },
 };
