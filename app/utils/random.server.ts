2c03221df8674e3b565b0da848cca256ec028224
Dov Alperin <git@dov.dev>
2022-03-27 14:19:20 -0400

    Add types to random.server.ts

--- app/utils/random.server.ts
+++ app/utils/random.server.ts
@@ -1,6 +1,6 @@
 import { randomBytes as crypto_randomBytes } from "crypto";
 
-export function genRandomID() {
+export function genRandomID(): string {
     const randomBytes = crypto_randomBytes(8);
     return Buffer.from(randomBytes).toString("hex");
 }
