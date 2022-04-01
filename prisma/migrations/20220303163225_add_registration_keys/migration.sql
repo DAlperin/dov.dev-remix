9235ede61467d6ad5d1ca6d5f2db469c5ed36bc9
Dov Alperin <git@dov.dev>
2022-04-01 11:08:39 -0400

    We rely on planetscale, not prisma for migrations now

--- prisma/migrations/20220303163225_add_registration_keys/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "registrationSecret" (
-    "key" TEXT NOT NULL,
-    "admin" BOOLEAN NOT NULL DEFAULT false
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "registrationSecret_key_key" ON "registrationSecret"("key");
--- prisma/migrations/20220305204950_add_used_to_registration_secret/migration.sql
+++
@@ -1,2 +0,0 @@
--- AlterTable
-ALTER TABLE "registrationSecret" ADD COLUMN     "used" BOOLEAN NOT NULL DEFAULT false;
