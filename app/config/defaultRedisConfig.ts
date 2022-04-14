32d92a027fdc1217ea293fc40e2084777a50b143
Dov Alperin <git@dov.dev>
2022-04-14 09:04:17 -0400

    Make redis geo-distributed to each region

--- app/config/defaultRedisConfig.ts
+++ app/config/defaultRedisConfig.ts
@@ -2,10 +2,15 @@ import type { RedisOptions } from "ioredis";
 
 import { assertedEnvVar } from "~/utils/environment.server";
 
+const baseHost = assertedEnvVar("REDIS_HOST")
+const region = assertedEnvVar("FLY_REGION")
+
+const host = `${region}.${baseHost}`
+
 const defaultRedisConfig: RedisOptions = {
     port: 6379,
     family: 6,
-    host: assertedEnvVar("REDIS_HOST"),
+    host,
     password: process.env.REDIS_PASSWORD ?? undefined,
 }
 
