89fa4fe1fca31a53fa83387e066dd7672397a125
Dov Alperin <git@dov.dev>
2022-04-23 22:00:43 -0400

    Revert to react 17 for now.
    Tracking in:
    https://github.com/remix-run/remix/issues/2570
    https://github.com/remix-run/remix/issues/2947

--- app/entry.server.tsx
+++ app/entry.server.tsx
@@ -1,54 +1,22 @@
-/* eslint-disable no-console */
-import type { Headers } from "@remix-run/node";
-import { Response } from "@remix-run/node";
-import isbot from "isbot";
-// @ts-expect-error react types havent caught up to react 18 yet
-import { renderToPipeableStream } from "react-dom/server";
-import { RemixServer } from "remix";
-import type { EntryContext } from "remix";
-import { PassThrough } from "stream";
-
-const ABORT_DELAY = 5000;
+import type { EntryContext } from "@remix-run/node";
+import { RemixServer } from "@remix-run/react";
+import { renderToString } from "react-dom/server";
 
 export default function handleRequest(
     request: Request,
     responseStatusCode: number,
     responseHeaders: Headers,
     remixContext: EntryContext
-): Promise<unknown> {
-    const callbackName = isbot(request.headers.get("user-agent"))
-        ? "onAllReady"
-        : "onShellReady";
+): Response {
+    const markup = renderToString(
+        // @ts-expect-error I can't really be bothered to figure out why typescript complains about this
+        <RemixServer context={remixContext} url={request.url} />
+    );
 
-    return new Promise((resolve, reject) => {
-        let didError = false;
+    responseHeaders.set("Content-Type", "text/html");
 
-        const { pipe, abort } = renderToPipeableStream(
-            <RemixServer context={remixContext} url={request.url} />,
-            {
-                [callbackName]() {
-                    const body = new PassThrough();
-
-                    responseHeaders.set("Content-Type", "text/html");
-
-                    resolve(
-                        new Response(body, {
-                            status: didError ? 500 : responseStatusCode,
-                            headers: responseHeaders,
-                        })
-                    );
-                    pipe(body);
-                },
-                onShellError(err: Error) {
-                    reject(err);
-                },
-                onError(error: Error) {
-                    didError = true;
-                    console.error(error);
-                },
-            }
-        );
-        // eslint-disable-next-line @typescript-eslint/no-implied-eval
-        setTimeout(abort, ABORT_DELAY);
+    return new Response(`<!DOCTYPE html>${markup}`, {
+        status: responseStatusCode,
+        headers: responseHeaders,
     });
 }
