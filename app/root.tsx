fd99584cf303cf047f2435f090bf42bee6586ea1
Dov Alperin <git@dov.dev>
2022-06-19 13:04:20 -0400

    Change where we load the analytics

--- app/root.tsx
+++ app/root.tsx
@@ -14,6 +14,7 @@ import type {
     LoaderFunction,
     MetaFunction,
 } from "@remix-run/server-runtime";
+import { load } from "fathom-client";
 import { AnimatePresence, motion } from "framer-motion";
 import { useEffect, useState } from "react";
 import { Outlet } from "remix";
